8f901b7b128e4d7cd907ebe0aab8eb38598b281c
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2020-11-19 23:40:16 -0500

    Refactor path/dir handling
    Execute all processes from within the modified directory.
    This will make post-install hooks easier to add (see issue #1).
    
    Remove aux functions `get_dir` and `is_pkg_dir`.
    They're called for every package, in the same place,
    and only once; might as well inline them.

--- lua/paq-nvim.lua
+++ lua/paq-nvim.lua
@@ -7,24 +7,16 @@ local REPO_RE = '^[%w-]+/([%w-_.]+)$'
 -- Table of 'name':{options} pairs
 local packages = {}
 
-local function get_dir(name, opt)
-    return PATH .. (opt and 'opt/' or 'start/') .. name
-end
-
-local function is_pkg_dir(dir)
-    return vim.fn.isdirectory(dir) ~= 0
-end
-
 local function print_res(action, args, ok)
     local res = ok and 'Paq: ' or 'Paq: Failed to '
     print(res .. action .. ' ' .. args)
 end
 
-local function call_git(action, name, ...)
+local function call_git(name, dir, action, ...)
     local args = {...}
     local handle
     handle = loop.spawn('git',
-        {args=args},
+        {args=args, cwd=dir},
         vim.schedule_wrap(
             function(code, signal)
                 print_res(action, name, code == 0)
@@ -34,25 +26,29 @@ local function call_git(action, name, ...)
     )
 end
 
-local function install_pkg(name, dir, args)
-    if not is_pkg_dir(dir) then
+local function install_pkg(name, dir, isdir, args)
+    if not isdir then
+        loop.fs_mkdir(dir, loop.fs_stat(PATH).mode)
         if args.branch then
-            call_git('install', name, 'clone', args.url, '-b',  args.branch, '--single-branch', dir)
+            call_git(name, dir, 'install', 'clone', args.url, '-b',  args.branch, '--single-branch', '.')
         else
-            call_git('install', name, 'clone', args.url, dir)
+            call_git(name, dir, 'install', 'clone', args.url, '.')
         end
     end
 end
 
-local function update_pkg(name, dir)
-    if is_pkg_dir(dir) then
-        call_git('update', name, '-C', dir, 'pull')
+local function update_pkg(name, dir, isdir)
+    if isdir then
+        call_git(name, dir, 'update', 'pull')
     end
 end
 
 local function map_pkgs(fn)
+    local dir, isdir
     for name, args in pairs(packages) do
-        fn(name, get_dir(name, args.opt), args)
+        dir = PATH .. (opt and 'opt/' or 'start/') .. name
+        isdir = vim.fn.isdirectory(dir) ~= 0
+        fn(name, dir, isdir, args)
     end
 end
 
@@ -62,7 +58,6 @@ local function rmdir(dir, ispkgdir)
     while handle do
         name, t = loop.fs_scandir_next(handle)
         if not name then break end
-
         child = dir .. '/' .. name
         if ispkgdir then --check which packages are listed
             if packages[name] then --do nothing
@@ -74,7 +69,6 @@ local function rmdir(dir, ispkgdir)
         else --it's an arbitrary directory or file
             ok = (t == 'directory') and rmdir(child) or loop.fs_unlink(child)
         end
-
         if not ok then return end
     end
     return ispkgdir or loop.fs_rmdir(dir) -- Don't delete start or opt
