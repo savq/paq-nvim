7b2ace7702e4ee4b02f54974845b99fa0474513a
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2020-12-18 17:58:52 -0500

    Refactor: Use better name conventions et al
    * Remove the distinction between the name of the command that runs and
      the name the user sees. Every user is a grown up, there's no point in
      calling `git pull` and update. That just adds more string clutter.
    
    * Use better name conventions in general (process and command where
      appropriate).
    
    * Split hook function into two.

--- lua/paq-nvim.lua
+++ lua/paq-nvim.lua
@@ -5,71 +5,71 @@ local REPO_RE = '^[%w-]+/([%w-_.]+)$'
 
 local uv = vim.loop -- Alias for Neovim's event loop (libuv)
 local packages = {} -- Table of 'name':{options} pairs
-local call_cmd      -- To handle mutual funtion recursion (Lua shenanigans)
+local run_hook      -- To handle mutual funtion recursion
 
-local function print_res(action, args, ok)
+local function print_res(cmd, name, ok)
     local res = ok and 'Paq: ' or 'Paq: Failed to '
-    print(res .. action .. ' ' .. args)
+    print(res .. cmd .. ' ' .. name)
     return ok
 end
 
-local function run_hook(hook, name, dir)
-    local t = type(hook)
-    if t == 'function' then
-        vim.cmd('packloadall!')
-        local ok = pcall(hook)
-        print_res('run hook for', name, ok) -- FIXME: How to print an interned string?
-
-    elseif t == 'string' then
-        local hook_args = {}
-        local hook_cmd
-        for word in hook:gmatch("%S+") do
-            table.insert(hook_args, word)
-        end
-        hook_cmd = table.remove(hook_args, 1)
-        call_cmd(hook_cmd, name, dir, "run `"..hook.."` for", hook_args)
-    end
-end
-
-function call_cmd(cmd, name, dir, action, args, hook)
-    local handle
+function call_proc(process, pkg, args, cwd)
+    local handle, t
     handle =
-        uv.spawn(cmd,
-            {args=args, cwd=(action ~= 'install' and dir or nil)}, -- FIXME: Handle install case better
-            vim.schedule_wrap(
-                function(code)
-                    print_res(action, name, code == 0)
-                    if hook then run_hook(hook, name, dir) end
-                    handle:close()
+        uv.spawn(process, {args=args, cwd=cwd},
+            vim.schedule_wrap( function (code)
+                print_res(args[1] or process, pkg.name, code == 0)
+                t = type(pkg.hook)
+                if t = 'function' then
+                    run_fn_hook(pkg.name, pkg.hook)
+                else if t = 'string' then
+                    run_shell_hook(pkg)
                 end
-            )
+                handle:close()
+            end)
         )
 end
 
+local function run_fn_hook(name, hook)
+    vim.cmd('packloadall!')
+    local ok = pcall(hook)
+    print_res('run hook for', name, ok)
+end
+
+local function run_shell_hook(pkg)
+    local process
+    local args = {}
+    for word in pkg.hook:gmatch("%S+") do
+        table.insert(args, word)
+    end
+    process = table.remove(hook_args, 1)
+    call_proc(process, pkg, args, pkg.dir)
+end
+
 local function install_pkg(pkg)
-    if pkg.exists then return end
     local install_args = {'clone', pkg.url}
+    if pkg.exists then return end
     if pkg.branch then
         vim.list_extend(install_args, {'-b',  pkg.branch})
     end
     vim.list_extend(install_args, {pkg.dir})
-    call_cmd('git', pkg.name, pkg.dir, 'install', install_args, pkg.hook)
+    call_proc('git', pkg, install_args)
 end
 
 local function update_pkg(pkg)
     if not pkg.exists then
-        call_cmd('git', pkg.name, pkg.dir, 'update', {'pull'}, pkg.hook)
+        call_proc('git', pkg, {'pull'}, pkg.dir)
     end
 end
 
-local function rmdir(dir, ispkgdir)
+local function rmdir(dir, is_pack_dir) --pack_dir = start | opt
     local name, t, child, ok
     local handle = uv.fs_scandir(dir)
     while handle do
         name, t = uv.fs_scandir_next(handle)
         if not name then break end
         child = dir .. '/' .. name
-        if ispkgdir then --check which packages are listed
+        if is_pack_dir then --check which packages are listed
             if packages[name] then --do nothing
                 ok = true
             else --package isn't listed, remove it
@@ -81,7 +81,7 @@ local function rmdir(dir, ispkgdir)
         end
         if not ok then return end
     end
-    return ispkgdir or uv.fs_rmdir(dir) -- Don't delete start or opt
+    return is_pack_dir or uv.fs_rmdir(dir) --don't delete start/opt
 end
 
 local function paq(args)
@@ -96,7 +96,7 @@ local function paq(args)
         name   = reponame,
         branch = args.branch,
         dir    = dir,
-        exists = vim.fn.isdirectory(dir) ~= 0,
+        exists = (vim.fn.isdirectory(dir) ~= 0),
         hook   = args.hook,
         url    = args.url or GITHUB .. args[1] .. '.git',
     }
