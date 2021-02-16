87f8d96c89a9e6f6cfef08d2c9e9ee422016e784
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2021-02-16 15:41:44 -0500

    REFACTOR
    - Join `counters` and `msgs` tables into one `ops` table
    - Place nvim 0.5 replacements in a module to emphasize they're temporary
    - Unite `output_result` and `count_ops` to simplify control flow
    - Reformat call_proc for the umpteenth time
    - Minor changes to variables names

--- lua/paq-nvim.lua
+++ lua/paq-nvim.lua
@@ -9,23 +9,18 @@ local uv = vim.loop -- Alias for Neovim's event loop (libuv)
 local packages = {} -- Table of 'name':{options} pairs
 local run_hook      -- To handle mutual funtion recursion
 
-local msgs = {
-    clone = 'cloned',
-    pull = 'pulled changes for',
-    remove = 'removed',
-    ['run hook for'] = 'ran hook for',
-}
-
 local num_pkgs = 0
-local counters = {
-    clone = {ok = 0, fail = 0},
-    pull = {ok = 0, fail = 0},
-    remove = {ok = 0, fail = 0},
+local ops = {
+    clone            = {ok = 0, fail = 0, past = 'cloned'            },
+    pull             = {ok = 0, fail = 0, past = 'pulled changes for'},
+    remove           = {ok = 0, fail = 0, past = 'removed'           },
+    ['run hook for'] = {ok = 0, fail = 0, past = 'ran hook for'      },
 }
 
--- Helper functions to replace 0.5 features
 
-local function tbl_map(func, t)
+local _nvim = {} -- Helper functions to replace 0.5 features
+
+function _nvim.tbl_map(func, t) --> table
     if vim.api.nvim_call_function('has', {'nvim-0.5'}) == 1 then
         return vim.tbl_map(func, t)
     end
@@ -37,7 +32,7 @@ local function tbl_map(func, t)
 end
 
 -- Warning: This mutates dst!
-local function list_extend(dst, src, start, finish)
+function _nvim.list_extend(dst, src, start, finish)
     if vim.api.nvim_call_function('has', {'nvim-0.5'}) == 1 then
         return vim.list_extend(dst, src, start, finish)
     end
@@ -47,65 +42,52 @@ local function list_extend(dst, src, start, finish)
     return dst
 end
 
-
-local function inc(counter, result)
-    counters[counter][result] = counters[counter][result] + 1
-end
-
-local function output_result(num, total, operation, name, ok)
-    local result = ok and msgs[operation] or 'Failed to ' .. operation
-    print(string.format('Paq [%d/%d] %s %s', num, total, result, name))
-    return ok
-end
-
-local function count_ops(operation, name, ok)
-    local op = counters[operation]
+local function output_result(op, name, ok) -- TODO: rename this function
+    local c = ops[op]
     local result = ok and 'ok' or 'fail'
-    inc(operation, result)
-    output_result(op[result], num_pkgs, operation, name,  ok)
-    if op.ok + op.fail == num_pkgs then
-        op.ok, op.fail = 0, 0
+    c[result] = c[result] + 1 -- update count
+
+    local msg = ok and c.past or 'Failed to ' .. op
+    print(string.format('Paq [%d/%d] %s %s', c[result], num_pkgs, msg, name))
+
+    if c.ok + c.fail == num_pkgs then
+        c.ok, c.fail = 0, 0
         vim.api.nvim_command 'packloadall! | helptags ALL'
     end
-    return ok
 end
 
 local function call_proc(process, pkg, args, cwd, ishook)
-    local log, stderr, handle, ok
-
+    local log, stderr, handle, ok, op
     log = uv.fs_open(LOGFILE, 'a+', 0x1A4) -- FIXME: Write in terms of uv.constants
     stderr = uv.new_pipe(false)
     stderr:open(log)
 
-    handle =
-        uv.spawn(process,
-            {args=args, cwd=cwd, stdio = {nil, nil, stderr}},
-            vim.schedule_wrap( function (code)
-                uv.fs_write(log, '\n\n', -1) --space out error messages
-                stderr:close()
-                handle:close()
-                uv.fs_close(log)
-
-                ok = (code == 0)
-                if not ishook then
-                    run_hook(pkg)
-                    count_ops(args[1] or process, pkg.name, ok)
-                else --hooks aren't counted
-                    output_result(0, 0, 'run hook for', pkg.name, ok)
-                end
-            end)
-        )
+    handle = uv.spawn(
+        process,
+        {args=args, cwd=cwd, stdio = {nil, nil, stderr}},
+        vim.schedule_wrap( function (code)
+            uv.fs_write(log, '\n\n', -1) --space out error messages
+            uv.fs_close(log)
+            stderr:close()
+            handle:close()
+            op = ishook and 'run hook for' or args[1] or process
+            output_result(op, pkg.name, code == 0)
+            if not ishook then run_hook(pkg) end
+        end)
+    )
 end
 
 function run_hook(pkg) --(already defined as local)
-    local t = type(pkg.hook)
+    local t, process, args, ok
+    t = type(pkg.hook)
+
     if t == 'function' then
         local ok = pcall(pkg.hook)
-        output_result(0, 0, 'run hook for', pkg.name, ok)
+        output_result('run hook for', pkg.name, ok)
+
     elseif t == 'string' then
-        local process
-        local args = {}
-        for word in pkg.hook:gmatch("%S+") do
+        args = {}
+        for word in pkg.hook:gmatch('%S+') do
             table.insert(args, word)
         end
         process = table.remove(args, 1)
@@ -114,13 +96,15 @@ function run_hook(pkg) --(already defined as local)
 end
 
 local function install_pkg(pkg)
-    local install_args = {'clone', pkg.url}
-    if pkg.exists then return inc('clone', 'ok') end
-    if pkg.branch then
-        list_extend(install_args, {'-b',  pkg.branch})
+    local args = {'clone', pkg.url}
+    if pkg.exists then
+        ops['clone']['ok'] = ops['clone']['ok'] + 1
+        return
+    elseif pkg.branch then
+        _nvim.list_extend(args, {'-b',  pkg.branch})
     end
-    list_extend(install_args, {pkg.dir})
-    call_proc('git', pkg, install_args)
+    _nvim.list_extend(args, {pkg.dir})
+    call_proc('git', pkg, args)
 end
 
 local function update_pkg(pkg)
@@ -141,7 +125,7 @@ local function rmdir(dir, is_pack_dir) --pack_dir = start | opt
                 ok = true
             else --package isn't listed, remove it
                 ok = rmdir(child)
-                count_ops('remove', name, ok)
+                output_result('remove', name, ok)
             end
         else --it's an arbitrary directory or file
             ok = (t == 'directory') and rmdir(child) or uv.fs_unlink(child)
@@ -152,19 +136,18 @@ local function rmdir(dir, is_pack_dir) --pack_dir = start | opt
 end
 
 local function paq(args)
+    local name, dir
     if type(args) == 'string' then args = {args} end
 
     num_pkgs = num_pkgs + 1
 
-    local reponame = args.as or args[1]:match(REPO_RE)
-    if not reponame then
-        return output_result(num_pkgs, num_pkgs, 'parse', args[1])
-    end
+    name = args.as or args[1]:match(REPO_RE)
+    if not name then return output_result('parse', args[1]) end
 
-    local dir = PATH .. (args.opt and 'opt/' or 'start/') .. reponame
+    dir = PATH .. (args.opt and 'opt/' or 'start/') .. name
 
-    packages[reponame] = {
-        name   = reponame,
+    packages[name] = {
+        name   = name,
         branch = args.branch,
         dir    = dir,
         exists = (vim.api.nvim_call_function('isdirectory', {dir}) ~= 0),
@@ -174,16 +157,15 @@ local function paq(args)
 end
 
 local function setup(args)
-    assert(type(args) == 'table', 'Paq.setup takes a single table argument')
-
+    assert(type(args) == 'table')
     if type(args.path) == 'string' then
         PATH = args.path --FIXME: should probably rename PATH
     end
 end
 
 return {
-    install = function() tbl_map(install_pkg, packages) end,
-    update  = function() tbl_map(update_pkg, packages) end,
+    install = function() _nvim.tbl_map(install_pkg, packages) end,
+    update  = function() _nvim.tbl_map(update_pkg, packages) end,
     clean   = function() rmdir(PATH..'start', 1); rmdir(PATH..'opt', 1) end,
     setup   = setup,
     paq     = paq,
