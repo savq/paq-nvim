cd2811d8ce46ddef6d2890b2287adddc8dcbbee6
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2021-04-07 16:30:35 -0500

    Update string convetions

--- lua/paq-nvim.lua
+++ lua/paq-nvim.lua
@@ -3,60 +3,58 @@ local uv  = vim.loop --alias for Neovim's event loop (libuv)
 -- nvim 0.4 compatibility
 local cmd = vim.api.nvim_command
 local vfn = vim.api.nvim_call_function
-local compat = require('paq-nvim.compat')
+local compat = require("paq-nvim.compat")
 
 ----- Constants
-local LOGFILE = vfn('stdpath', {'cache'}) .. '/paq.log'
-local GITHUB = 'https://github.com/'
-local REPO_RE = '^[%w-]+/([%w-_.]+)$'
+local LOGFILE = vfn('stdpath', {"cache"}) .. "/paq.log"
 
 ----- Globals
 local run_hook; --to handle mutual recursion
-local paq_dir = vfn('stdpath', {'data'}) .. '/site/pack/paqs/'
-local packages = {} --table of 'name':{options} pairs
-local changes = {} --table of 'name':'change' pairs
-local num_pkgs = 0
+local paq_dir   = vfn('stdpath', {"data"}) .. "/site/pack/paqs/"
+local packages  = {} --table of 'name':{options} pairs
+local changes   = {} --table of 'name':'change' pairs
+local num_pkgs  = 0
 local num_to_rm = 0
 
 local ops = {
-    clone  = {ok=0, fail=0, past='cloned'},
-    pull   = {ok=0, fail=0, past='pulled changes for'},
-    remove = {ok=0, fail=0, past='removed'},
+    clone  = {ok=0, fail=0, past="cloned"},
+    pull   = {ok=0, fail=0, past="pulled changes for"},
+    remove = {ok=0, fail=0, past="removed"},
 }
 
 local function output_result(op, name, ok, ishook)
     local result, total, msg
-    local count = ''
-    local failstr = 'Failed to '
+    local count = ""
+    local failstr = "Failed to "
     local c = ops[op]
     if c then
         result = ok and 'ok' or 'fail'
         c[result] = c[result] + 1
-        total = (op == 'remove') and num_to_rm or num_pkgs -- FIXME
-        count = string.format('%d/%d', c[result], total)
+        total = (op == "remove") and num_to_rm or num_pkgs --FIXME
+        count = string.format("%d/%d", c[result], total)
         msg = ok and c.past or failstr .. op
         if c.ok + c.fail == total then  --no more packages to update
             c.ok, c.fail = 0, 0
-            cmd('packloadall! | helptags ALL')
+            cmd("packloadall! | helptags ALL")
         end
-    elseif ishook then --hooks aren't counted
-        msg = (ok and 'ran' or failstr .. 'run') .. string.format(' `%s` for', op)
+    elseif ishook then --hooks aren"t counted
+        msg = (ok and "ran" or failstr .. "run") .. string.format(" `%s` for", op)
     else
         msg = failstr .. op
     end
-    print(string.format('Paq [%s] %s %s', count, msg, name))
+    print(string.format("Paq [%s] %s %s", count, msg, name))
 end
 
 local function call_proc(process, pkg, args, cwd, ishook, cb)
     local log, stderr, handle
-    log = uv.fs_open(LOGFILE, 'a+', 0x1A4) -- FIXME: Write in terms of uv.constants
+    log = uv.fs_open(LOGFILE, 'a+', 0x1A4)
     stderr = uv.new_pipe(false)
     stderr:open(log)
     handle = uv.spawn(
         process,
         {args=args, cwd=cwd, stdio = {nil, nil, stderr}},
         vim.schedule_wrap( function(code)
-            uv.fs_write(log, '\n', -1) --space out error messages
+            uv.fs_write(log, "\n", -1) --space out error messages
             uv.fs_close(log)
             stderr:close()
             handle:close()
@@ -72,13 +70,13 @@ function run_hook(pkg) --(already defined as local)
     t = type(pkg.run)
 
     if t == 'function' then
-        cmd('packadd ' .. pkg.name)
+        cmd("packadd " .. pkg.name)
         ok = pcall(pkg.run)
         output_result(t, pkg.name, ok, true)
 
     elseif t == 'string' then
         args = {}
-        for word in pkg.run:gmatch('%S+') do
+        for word in pkg.run:gmatch("%S+") do
             table.insert(args, word)
         end
         process = table.remove(args, 1)
@@ -87,12 +85,12 @@ function run_hook(pkg) --(already defined as local)
 end
 
 local function install(pkg)
-    local args = {'clone', pkg.url}
+    local args = {"clone", pkg.url}
     if pkg.exists then
         ops['clone']['ok'] = ops['clone']['ok'] + 1
         return
     elseif pkg.branch then
-        compat.list_extend(args, {'-b',  pkg.branch})
+        compat.list_extend(args, {"-b",  pkg.branch})
     end
     compat.list_extend(args, {pkg.dir})
     local cb = function(code)
@@ -101,7 +99,7 @@ local function install(pkg)
             changes[pkg.name] = 'installed'
         end
     end
-    call_proc('git', pkg, args, nil, nil, cb)
+    call_proc("git", pkg, args, nil, nil, cb)
 end
 
 local function get_git_hash(dir)
@@ -127,7 +125,7 @@ local function update(pkg)
                 changes[pkg.name] = 'updated'
             end
         end
-        call_proc('git', pkg, {'pull'}, pkg.dir, nil, cb)
+        call_proc("git", pkg, {"pull"}, pkg.dir, nil, cb)
     end
 end
 
@@ -137,7 +135,7 @@ local function iter_dir(fn, dir, args)
     while handle do
         name, t = uv.fs_scandir_next(handle)
         if not name then break end
-        child = dir .. '/' .. name
+        child = dir .. "/" .. name
         ok = fn(child, name, t, args)
         if not ok then return end
     end
@@ -163,12 +161,12 @@ end
 local function clean_pkgs()
     local ok
     local rm_list = {}
-    iter_dir(mark_dir, paq_dir .. 'start', rm_list)
-    iter_dir(mark_dir, paq_dir .. 'opt', rm_list)
-    num_to_rm = #rm_list    -- update count of plugins to be deleted
+    iter_dir(mark_dir, paq_dir .. "start", rm_list)
+    iter_dir(mark_dir, paq_dir .. "opt", rm_list)
+    num_to_rm = #rm_list    --update count of plugins to be deleted
     for _, i in ipairs(rm_list) do
         ok = iter_dir(rm_dir, i.dir) and uv.fs_rmdir(i.dir)
-        output_result('remove', i.name, ok)
+        output_result("remove", i.name, ok)
         if ok then changes[i.name] = 'removed' end
     end
 end
@@ -177,10 +175,10 @@ local function paq(args)
     local name, dir
     if type(args) == 'string' then args = {args} end
 
-    name = args.as or args[1]:match(REPO_RE)
-    if not name then return output_result('parse', args[1]) end
+    name = args.as or args[1]:match("^[%w-]+/([%w-_.]+)$")
+    if not name then return output_result("parse", args[1]) end
 
-    dir = paq_dir .. (args.opt and 'opt/' or 'start/') .. name
+    dir = paq_dir .. (args.opt and "opt/" or "start/") .. name
 
     if not packages[name] then
         num_pkgs = num_pkgs + 1
@@ -192,14 +190,14 @@ local function paq(args)
         dir    = dir,
         exists = (vfn('isdirectory', {dir}) ~= 0),
         run    = args.run or args.hook, --wait for paq 1.0 to deprecate
-        url    = args.url or GITHUB .. args[1] .. '.git',
+        url    = args.url or "https://github.com/" .. args[1] .. ".git",
     }
 end
 
 local function setup(args)
     assert(type(args) == 'table')
     if type(args.path) == 'string' then
-        paq_dir = args.path --FIXME: should probably rename paq_dir
+        paq_dir = args.path
     end
 end
 
@@ -212,7 +210,6 @@ local function list()
     table.sort(installed)
     table.sort(removed)
 
-    local indent = "   "
     local symb_tbl = {
         installed = "+",
         updated   = "*",
@@ -221,7 +218,7 @@ local function list()
     }
 
     local function prefix(name)
-        return indent .. symb_tbl[changes[name] or 'default'] .. name
+        return "   " .. (symb_tbl[changes[name]] or " ") .. name
     end
 
     local function list_pkgs(header, pkgs)
@@ -240,6 +237,6 @@ return {
     list      = list,
     setup     = setup,
     paq       = paq,
-    log_open  = function() cmd('sp ' .. LOGFILE) end,
-    log_clean = function() uv.fs_unlink(LOGFILE); print('Paq log file deleted') end,
+    log_open  = function() cmd("sp " .. LOGFILE) end,
+    log_clean = function() uv.fs_unlink(LOGFILE); print("Paq log file deleted") end,
 }
