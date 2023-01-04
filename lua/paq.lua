07407b2fcda4d049b2bdd58d658bfc59e1e5aa22
Luca Saccarola <96259932+saccarosium@users.noreply.github.com>
2023-01-04 03:29:50 +0100

    feat: opt in setup function as default plugin location instead of start (#132)

--- lua/paq.lua
+++ lua/paq.lua
@@ -1,6 +1,7 @@
 local uv = vim.loop
 local cfg = {
     path = vim.fn.stdpath("data") .. "/site/pack/paqs/",
+    opt = false,
     verbose = false,
 }
 local logpath = vim.fn.has("nvim-0.8") == 1 and vim.fn.stdpath("log") or vim.fn.stdpath("cache")
@@ -276,7 +277,8 @@ local function register(args)
     elseif packages[name] then
         return
     end
-    local dir = cfg.path .. (args.opt and "opt/" or "start/") .. name
+    local opt = (args.opt == nil or args.opt) and cfg.opt
+    local dir = cfg.path .. (opt and "opt/" or "start/") .. name
     packages[name] = {
         name = name,
         branch = args.branch,
